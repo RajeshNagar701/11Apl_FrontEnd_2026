34f0db1852904a04740e78ffb079b66c940cde52
RajeshNagar701 <rajeshnagar.tops@gmail.com>
2026-04-18 11:52:16 +0530

    change

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Class_component from "./component/Class_component";
+import Function_component from "./component/Function_component";
+import Layout from "./layout/Layout";
+
+
+function App() {
+  return (
+    <div>
+      {
+        //<Function_component/>
+        //<Class_component/>
+
+        <Layout/>
+      }
+        
+    </div>
+  );
+}
+
+export default App;
