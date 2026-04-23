6ff6e769cfd43de636610b2d2a18039fbdff5dd9
RajeshNagar701 <rajeshnagar.tops@gmail.com>
2026-04-23 11:46:49 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Class_component from "./component/Class_component";
 import Function_component from "./component/Function_component";
+import Css_apply from "./css/Css_apply";
+import Jsx_component from "./Jsx/Jsx_component";
 import Layout from "./layout/Layout";
+import Module_css from "./Module_css/Module_css";
 
 
 function App() {
@@ -10,9 +13,12 @@ function App() {
         //<Function_component/>
         //<Class_component/>
 
-        <Layout/>
+        //<Layout/>
+        //<Jsx_component/>
+
+        //<Css_apply/>
       }
-        
+        <Module_css/>
     </div>
   );
 }
